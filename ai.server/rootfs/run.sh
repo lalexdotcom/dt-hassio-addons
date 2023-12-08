76694ee340e84a7c04614211c9f0ff8017fe0030
LAlex <lalex@lalex.com>
2023-12-08 21:11:32 +0100

    Ingress and CLI option for modules path

--- ai.server/rootfs/run.sh
+++ ai.server/rootfs/run.sh
@@ -6,10 +6,9 @@ export DATA_DIR=$(jq --raw-output '.DATA_DIR // empty' $CONFIG_PATH)
 mkdir -p "$MODULES_PATH"
 mkdir -p "$DATA_DIR"
 
-rm -rf /app/modules
-ln -sf "$MODULES_PATH" /app/modules
-
 # cat /app/server/appsettings.json | sed -e 's/\/\/ .*$//g' | jq ".ModuleOptions.ModulesPath=\"$MODULES_PATH\"" > /app/server/appsettings.json.new
 # mv /app/server/appsettings.json.new /app/server/appsettings.json
 
-cd /app/server && dotnet ./CodeProject.AI.Server.dll --ApplicationDataDir="$DATA_DIR"
+/usr/sbin/nginx
+
+cd /app/server && dotnet ./CodeProject.AI.Server.dll --ApplicationDataDir="$DATA_DIR" --ModuleOptions:ModulesPath="$MODULES_PATH"
