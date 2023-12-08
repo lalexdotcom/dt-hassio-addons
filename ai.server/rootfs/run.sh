cba554b4ae07d673f4745effa60563215dce114a
LAlex <lalex@lalex.com>
2023-12-08 16:07:02 +0100

    No ModulesPath option

--- ai.server/rootfs/run.sh
+++ ai.server/rootfs/run.sh
@@ -9,7 +9,7 @@ mkdir -p "$DATA_DIR"
 rm -rf /app/modules
 ln -sf "$MODULES_PATH" /app/modules
 
-cat /app/server/appsettings.json | sed -e 's/\/\/ .*$//g' | jq ".ModuleOptions.ModulesPath=\"$MODULES_PATH\"" > /app/server/appsettings.json.new
-mv /app/server/appsettings.json.new /app/server/appsettings.json
+# cat /app/server/appsettings.json | sed -e 's/\/\/ .*$//g' | jq ".ModuleOptions.ModulesPath=\"$MODULES_PATH\"" > /app/server/appsettings.json.new
+# mv /app/server/appsettings.json.new /app/server/appsettings.json
 
 cd /app/server && dotnet ./CodeProject.AI.Server.dll --ApplicationDataDir="$DATA_DIR"
